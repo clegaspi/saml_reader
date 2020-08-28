5a4404ed74438b07309b162bca6a69b933542c0d
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2020-08-27 22:29:14 -0700

    Docstrings for cli.py

--- saml_reader/cli.py
+++ saml_reader/cli.py
@@ -1,3 +1,12 @@
+"""
+Command line interface for SAML Reader parser. This is where MongoDB Cloud-specific
+interpretation of the SAML data is done and returned to the user.
+
+Attributes:
+    REQUIRED_ATTRIBUTES (set): set of strings of the required Atlas attributes in the
+        SAML response
+    VALID_INPUT_TYPES (set): set of strings of the valid input types for this tool
+"""
 import sys
 
 import pyperclip
@@ -14,6 +23,18 @@ VALID_INPUT_TYPES = {'base64', 'xml', 'har'}
 
 
 def read_file(filename):
+    """
+    Reads data from a file
+
+    Args:
+        filename (basestring): path of file to read
+
+    Returns:
+        (basestring) contents of file
+
+    Raises:
+        (FileNotFoundError) if the file does not exist or cannot be read
+    """
     try:
         with open(filename, 'r') as f:
             data = f.read()
@@ -23,16 +44,42 @@ def read_file(filename):
 
 
 def read_clipboard():
+    """
+    Reads data from the system clipboard
+
+    Returns:
+        (basestring) contents of clipboard
+    """
     data = pyperclip.paste()
     return data
 
 
 def read_stdin():
+    """
+    Reads contents of stdin (standard in) to get piped data
+
+    Returns:
+        (basestring) concatenated contents of stdin
+    """
     data = "".join(sys.stdin.readlines())
     return data
 
 
 def parse_raw_data(input_type, data):
+    """
+    Parse various data types to return SAML response
+
+    Args:
+        input_type (basestring): data type of `data`, must be
+            `'base64'`, `'xml'`, or `'har'`
+        data (basestring): data to parse for SAML response
+
+    Returns:
+        (SamlParser) Object containing SAML data
+
+    Raises:
+        (ValueError) if an invalid `input_type` is specified
+    """
     if input_type == 'base64':
         return SamlParser(data)
     if input_type == 'xml':
@@ -43,6 +90,25 @@ def parse_raw_data(input_type, data):
 
 
 def parse(source, input_type, filename=None):
+    """
+    Parses input for SAML response and displays summary and analysis
+
+    Args:
+        source (basestring): type of input to read, must be one of:
+            - `'clip'`: read from the system clipboard
+            - `'file'`: read from a file, must specify path in `'filename'`
+            - `'stdin'`: read from pipe via stdin (standard in)
+        input_type (basestring): data type of `data`, must be
+            `'base64'`, `'xml'`, or `'har'`
+        filename (basestring, Optional): path of file to read, only required
+            if `source` is `'file'`
+
+    Returns:
+        None
+
+    Raises:
+        (ValueError) if the `source` or the `input_type` is invalid
+    """
     input_type = input_type.lower()
     if input_type not in VALID_INPUT_TYPES:
         raise ValueError(f"Invalid input type: {input_type}")
@@ -73,6 +139,7 @@ def parse(source, input_type, filename=None):
         return
 
     try:
+        # TODO: Maybe move this to the display() section or wherever it ends up in a refactor
         cert = Certificate(saml.get_certificate())
     except ValueError:
         print("Could not locate certificate. Identity provider info will not be available.")
@@ -81,6 +148,21 @@ def parse(source, input_type, filename=None):
 
 
 def display(saml, cert):
+    """
+    Display parsed SAML data and MongoDB Cloud-specific recommendations for identifiable issues
+    with the SAML data.
+
+    Args:
+        saml (SamlParser): SAML data
+        cert (Certificate, NoneType): Certificate data. Can be `None` if no certificate info is
+            available in the SAML response
+
+    Returns:
+        None
+    """
+
+    # TODO: It might be nice to abstract this into separate functions or a class
+    #       to put all of the MongoDB Cloud-specific analysis in one place
     print(f"SAML READER")
     print(f"----------------------")
     if cert is not None:
@@ -102,6 +184,8 @@ def display(saml, cert):
           f"\nFormat (this should end in 'unspecified' or 'emailAddress'): "
           f"{saml.get_subject_nameid_format()}")
     print("---")
+
+    # Checking for the required attributes for MongoDB Cloud
     print(f"ATTRIBUTES:")
     req_attribs_in_assertion = set()
     for name, value in saml.get_attributes().items():
@@ -119,10 +203,17 @@ def display(saml, cert):
 
 
 def cli():
+    """
+    Entrypoint for the command line interface. Handles parsing command line arguments.
+
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(prog="SAML Reader",
                                      description='Read a SAML response and pull out '
                                                  'relevant values for diagnosing '
                                                  'federated authentication issues.')
+    # TODO: Look into having argparse verify if the path is valid
     parser.add_argument('filepath', metavar="PATH", action='store',
                         default=None, nargs='?',
                         help='path for source file. If omitted, '
