ad5c48cd103eb15cb156151577224d8cdaf77067
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2021-03-29 09:40:13 -0700

    Hotfix for ACS (#40)

--- saml_reader/saml/parser.py
+++ saml_reader/saml/parser.py
@@ -86,7 +86,7 @@ class StandardSamlParser(BaseSamlParser):
             'certificate': lambda x: x[0].text if x else None,
             'name_id': lambda x: x[0].text if x else None,
             'name_id_format': lambda x: x[0].attrib.get('Format') if x else None,
-            'acs': lambda x: x[0][0].attrib.get('Destination') or x[0][1].attrib.get('Recipient') or None,
+            'acs': lambda x: x[0][0].attrib.get('Destination') or x[1][0].attrib.get('Recipient') or None,
             'encryption': self.__parse_encryption,
             'audience': lambda x: x[0].text if x else None,
             'issuer': lambda x: x[0].text if x else None,
