51f44c686f257b8f6fbdd842abd5cafd34ec2557
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2021-04-03 16:56:39 -0700

    Remove unneeded code from web app

--- web/app.py
+++ web/app.py
@@ -2,15 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-# from flask_caching import Cache
-# import plotly.io as pio
 
 from web.home import home_layout
 from web.analyze import analyze_layout
 
 app = dash.Dash(__name__)
 server = app.server
-# app.config.suppress_callback_exceptions = True  # TODO: remove this?
 app.scripts.config.serve_locally = True
 app.title = "SAML Reader"
 route = dcc.Location(id='url', refresh=False)
@@ -28,19 +25,10 @@ app.layout = html.Div(
               html.Div(id='page-content')],
     style={'marginLeft': 200, 'marginRight': 200, 'marginTop': 30})
 
-#CORRELATE_LAYOUT = correlate_layout(app)
-#INTERACTIVE_LAYOUT = interactive_layout(app)
-#EXPLORE_LAYOUT = explore_layout(app)
-#REFS_LAYOUT = refs_layout(app)
-#define_plot_callbacks(app)
+# Initialize page layouts to register callbacks
 
-# routing, current routes defined are:
-# / for home page
-# /model for model summary
-# /model/model_name for information on that model
-# /property for property summary
-# /property/property_name for information on that property
-
+# Uncomment this if you define callbacks for objects that don't exist yet
+# app.config.suppress_callback_exceptions = True
 HOME_LAYOUT = home_layout(app)
 ANALYZE_LAYOUT = analyze_layout(app)
 
@@ -63,5 +51,4 @@ def display_page(pathname, search):
 
 
 if __name__ == '__main__':
-    # app.run_server(debug=False, dev_tools_ui=True)
     app.run_server(debug=False, dev_tools_ui=True)
