a7d3eaf0fccd1d9c96ed1d70a946a29a43d1de02
clegaspi <1392774+clegaspi@users.noreply.github.com>
2021-08-06 21:43:12 -0700

    change cert date to date() type

--- saml_reader/validation/input_validation.py
+++ saml_reader/validation/input_validation.py
@@ -73,7 +73,7 @@ class UserInputParser:
             'lastName': lambda x: x.strip(),
             'email': lambda x: x.strip(),
             'role_mapping_expected': lambda x: x.upper() == 'Y',
-            'cert_expiration': lambda x: datetime.strptime(x, '%m/%d/%Y')
+            'cert_expiration': lambda x: datetime.strptime(x, '%m/%d/%Y').date()
         }
 
     def parse(self, attribute_name, value):
